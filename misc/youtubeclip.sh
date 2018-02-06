2ae9f16f6c0d459a6d9d88ad600931c377f2532a
beleriandcrises <beleriandcrises@pokemail.net>
2018-02-06 13:37:50 +0100

    added comments

--- misc/youtubeclip.sh
+++ misc/youtubeclip.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 # This script will download  the youtube video from  your clipboard
+#
+# To add a keybind so that pressing Windows + Alt + y will download the video add this to your ~/.config/openbox/rc.xml
+# <keybind key="W-A-y">
+#  <action name="Execute">  
+#     <command>~/path/to/youtubeclip.sh</command>
+#  </action>
+# </keybind>
+
 youtube-dl -x --audio-format vorbis -o "~/Music/YoutubeDL/%(title)s.%(ext)s" $(clipit -c) ; notify-send "Audio downloaded"
